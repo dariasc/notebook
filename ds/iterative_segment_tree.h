35d554cff76fb917d412f9c3d4cf98a6ad47a728
Diego Arias <dariascauas@gmail.com>
2024-04-23 23:54:38 -0400

    feat: add build() and remove unit from segment tree

--- ds/iterative_segment_tree.h
+++ ds/iterative_segment_tree.h
@@ -3,22 +3,40 @@
 name = "iterative_segment_tree"
 [info]
 - */
-template <class T, T unit, T F(T, T)> struct segment_tree {
-  vector<T> s;
+template <class T, T F(T, T)> struct segment_tree {
+  vec<T> s;
   int n;
-  segment_tree(int n = 0, T def = unit) : s(2 * n, def), n(n) {}
+  segment_tree(int n = 0) : s(2 * n), n(n) {}
+  void build(vec<T> a) {
+    for (int i = 0; i < n; i++)
+      s[i + n] = a[i];
+    for (int i = n - 1; i > 0; --i)
+      s[i] = F(s[i * 2], s[i * 2 + 1]);
+  }
   void update(int pos, T val) {
     for (s[pos += n] = val; pos /= 2;)
       s[pos] = F(s[pos * 2], s[pos * 2 + 1]);
   }
+  T merge(T a, T b, bool ua, bool ub) {
+    if (!ub)
+      return a;
+    if (!ua)
+      return b;
+    return F(a, b);
+  }
   T query(int b, int e) { // query [b, e)
-    T ra = unit, rb = unit;
+    bool ua = 0, ub = 0;
+    T ra, rb;
     for (b += n, e += n; b < e; b /= 2, e /= 2) {
-      if (b % 2)
-        ra = F(ra, s[b++]);
-      if (e % 2)
-        rb = F(s[--e], rb);
+      if (b % 2) {
+        ra = merge(ra, s[b++], ua, 1);
+        ua = 1;
+      }
+      if (e % 2) {
+        rb = merge(s[--e], rb, 1, ub);
+        ub = 1;
+      }
     }
-    return F(ra, rb);
+    return merge(ra, rb, ua, ub);
   }
 };
