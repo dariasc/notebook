6417dc553b1eca4c07bd37a7cb7d54d16b2c9393
Diego Arias <dariascauas@gmail.com>
2025-02-03 17:10:33 -0300

    feat: add fenwick tree test

---
+++ tests/ds/fenwick_tree.test.cpp
@@ -0,0 +1,27 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/point_add_range_sum" 
+#include "../../lib/template.h"
+#include "../../lib/ds/value.h"
+#include "../../lib/ds/fenwick_tree.h"
+
+int main() {
+  cin.tie(0)->sync_with_stdio(0);
+  cin.exceptions(cin.failbit);
+  int n, q;
+  cin >> n >> q;
+  vec<int> a(n);
+  FT tree(n);
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+    tree.update(i, a[i]);
+  }
+  while (q--) {
+    int t, a, b;
+    cin >> t >> a >> b;
+    if (t == 0) {
+      tree.update(a, b);
+    } else if (t == 1) {
+      cout << tree.query(b) - tree.query(a)  << "\n";
+    }
+  }
+}
+
