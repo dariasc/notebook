607bb1f5efa446a57eee8001e5de781fad884264
mitu <111256694+m1tuw@users.noreply.github.com>
2025-03-12 05:02:02 -0300

    changed old implementation by tested halfplane on pain in the ass problem

--- lib/misc/halfplane.cpp
+++ lib/misc/halfplane.cpp
@@ -1,36 +1,46 @@
 /**
- el vasito icpc implementation adapted to kactl template
+Halfplane intersection, works in O(nlogn)
+Status: tested in https://codeforces.com/gym/595381/problem/A
+Halfplanes are defined by a vector starting from p with direction pq,
+covering the left of this line.
 */
-struct Line {
-	P p, q;
-	double a;
-	Line() {}
-	Line(P p, P q) : p(p), q(q), a((q - p).angle()) {}
-	bool operator<(Line o) const { return a < o.a; }
+struct Halfplane {
+	P p, pq;
+    long double angle;
+    Halfplane() {}
+    Halfplane(const P&a, const P&b){ p = a; pq = b; angle = atan2l(pq.y, pq.x); }
+    bool out(const P& r) { return pq.cross(r-p) < -eps; }
+    bool operator<(const Halfplane& e) const { return angle < e.angle; }
+    friend P inter(const Halfplane& s, const Halfplane& t){
+        long double alpha = (t.p-s.p).cross(t.pq)/s.pq.cross(t.pq);
+        return s.p + (s.pq*alpha);
+    }   
 };
-#define L(a) a.p, a.q
-#define PQ(a) (a.q - a.p)
-#define ang(a) (a.p - a.q).angle()
-vector<P> halfPlaneIntersection(vector<Line> v) {
-	sort(all(v));
-	ll n = sz(v), q = 1, h = 0;
-	const long double eps = 1e-18;
-	vector<Line> c(n+2);
-	#define I(j, k) lineInter(L(c[j]), L(c[k])).se  
-	rep(i, 0, n) {
-		while (q < h && sideOf(L(v[i]), I(h, h-1), eps) < 0) h--;
-		while (q < h && sideOf(L(v[i]), I(q, q+1), eps) < 0) q++;
-		c[++h] = v[i];
-		if (q < h && abs(PQ(c[h]).cross(PQ(c[h-1]))) < eps) {
-			if (PQ(c[h]).dot(PQ(c[h-1])) <= 0) return {};
-			if (sideOf(L(v[i]), c[--h].p, eps) < 0) c[h] = v[i];
-		}
-	}
-	while (q < h - 1 && sideOf(L(c[q]), I(h, h-1), eps) < 0) h--;
-	while (q < h - 1 && sideOf(L(c[h]), I(q, q+1), eps) < 0) q++;
-	if (h - q <= 1) return {};
-	c[++h] = c[q];
-	vector<P> s;
-	rep(i, q, h) s.pb(I(i, i+1));
-	return s;
+vector<P> halfPlaneIntersection(vector<Halfplane>& H) { 
+    P box[4] = { P(inf, inf), P(-inf, inf), P(-inf, -inf), P(inf, -inf) };
+    rep(i,0,4) { // Add bounding box half-planes.
+        Halfplane aux(box[i], box[(i+1) % 4] - box[i]);
+        H.push_back(aux);
+    }
+    sort(H.begin(), H.end());
+    deque<Halfplane> dq;
+    int len = 0;
+    rep(i,0,sz(H)) {
+        while (len > 1 && H[i].out(inter(dq[len-1], dq[len-2]))) { dq.pop_back(); --len; }
+        while (len > 1 && H[i].out(inter(dq[0], dq[1]))) { dq.pop_front(); --len; }
+        if (len > 0 && fabsl(H[i].pq.cross(dq[len-1].pq)) < eps) {
+            if (H[i].pq.dot(dq[len-1].pq) < 0.0) return vector<P>();
+            if (H[i].out(dq[len-1].p)) { dq.pop_back(); --len; }
+            else continue;
+        }
+        dq.push_back(H[i]);
+        ++len;
+    }
+    while (len>2 && dq[0].out(inter(dq[len-1],dq[len-2]))) { dq.pop_back(); --len; }
+    while (len>2 && dq[len-1].out(inter(dq[0],dq[1]))) { dq.pop_front(); --len; }
+    if (len<3) return vector<P>();
+    vector<P> ret(len);
+    rep(i,0,len-1) { ret[i] = inter(dq[i], dq[i+1]); }
+    ret.back() = inter(dq[len-1], dq[0]);
+    return ret;
 }
