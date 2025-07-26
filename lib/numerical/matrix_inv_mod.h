e49138ed6df0c7278d90b222547cd2c30e85ed0f
Diego Arias <git@dariasc.com>
2025-07-26 18:15:47 -0400

    fix: small typo in matrix inv

--- lib/numerical/matrix_inv_mod.h
+++ lib/numerical/matrix_inv_mod.h
@@ -3,7 +3,7 @@
 /* -
 name = "Matrix Inverse ($mod p$)"
 [info]
-description = "Invert matrix $A$ modulo a prime. Returns $\"rank\"$; result is stored in $A$ unless singular ($\"rank\" < n$). For prime powers, repeatedly set $A^(-1) = A^(-1) (2I - AA^(-1)) space (mod p^k)$ where $A^(-1)$ starts as the inverse of $A mod p$, and $k$ is doubled in each step."
+description = "Invert matrix $A$ modulo a prime. Returns $\"rank\"$; result is stored in $A$ unless singular ($\"rank\" < n$). For prime powers, repeatedly set $A^(-1) = A^(-1) (2I - A A^(-1)) space (mod p^k)$ where $A^(-1)$ starts as the inverse of $A mod p$, and $k$ is doubled in each step."
 time = "$O(n^3)$"
 - */
 int matInv(vec<vec<ll>>& A) {
