78f04ecb19699d2bda8a67cd9ca5f7768825feb6
Diego Arias <git@dariasc.com>
2026-01-27 11:31:28 -0300

    fix: include in iterative and recursive st

--- lib/ds/st/iterative_segment_tree.h
+++ lib/ds/st/iterative_segment_tree.h
@@ -1,4 +1,4 @@
-#include "../template.h"
+#include "../../template.h"
 /* -
 name = "Iterative Segment Tree"
 [info]
--- lib/ds/st/recursive_segment_tree.h
+++ lib/ds/st/recursive_segment_tree.h
@@ -1,4 +1,4 @@
-#include "../template.h"
+#include "../../template.h"
 #include "split.h"
 /* -
 name = "Segment Tree"
