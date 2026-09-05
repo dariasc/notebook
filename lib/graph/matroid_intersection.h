fcec197da69dfed0866cc2275a6f99cfd0a6c6e5
Diego Arias <git@dariasc.com>
2026-09-05 19:34:52 -0400

    refactor: format matroid intersection and ntt

--- lib/graph/matroid_intersection.h
+++ lib/graph/matroid_intersection.h
@@ -6,14 +6,13 @@ source = "https://github.com/brunomaletta/Biblioteca/blob/master/Codigo/Primitiv
 description = "Algorithms for finding maximum independent sets in the intersection of two matroids. `oracle(i)` is independent after adding $i$, `oracle(i, j)` ... after swapping $i$, $j$."
 time = "$O(r^2 n)$ for unweighted. For weighted $O(r^3 n)$ with SPFA $O(r^2 n log n)$ with Dijkstra with potentials"
 - */
-template<typename Matroid1, typename Matroid2>
-vi matroidIntersection(int n, Matroid1 M1, Matroid2 M2) {
+vi matroidIntersection(int n, auto M1, auto M2) {
   vec<bool> b(n);
   vi I[2];
   bool converged = false;
   while (!converged) {
     I[0].clear(), I[1].clear();
-    for (int u = 0; u < n; u++) I[b[u]].push_back(u);
+    rep(u,0,n) I[b[u]].push_back(u);
     M1.build(I[1]), M2.build(I[1]);
     vec<bool> target(n), pushed(n);
     queue<int> q;
@@ -23,7 +22,7 @@ vi matroidIntersection(int n, Matroid1 M1, Matroid2 M2) {
     }
     vi p(n, -1);
     converged = true;
-    while (q.size()) {
+    while (sz(q)) {
       int u = q.front(); q.pop();
       if (target[u]) {
         converged = false;
@@ -31,33 +30,32 @@ vi matroidIntersection(int n, Matroid1 M1, Matroid2 M2) {
         break;
       }
       for (int v : I[!b[u]]) if (!pushed[v]) {
-        if ((b[u] and M1.oracle(u, v)) ||
-            (b[v] and M2.oracle(v, u)))
+        if ((b[u] && M1.oracle(u, v)) ||
+            (b[v] && M2.oracle(v, u)))
           p[v] = u, pushed[v] = true, q.push(v);
       }
     }
   }
   return I[1];
 }
-template<typename T, typename Matroid1, typename Matroid2>
+template <class T>
 vi weightedMatroidIntersection(
-  int n, vec<T> w, Matroid1 M1, Matroid2 M2
+  int n, vec<T> w, auto M1, auto M2
 ) {
   vec<bool> b(n), target(n), is_inside(n);
   vi I[2], from(n);
   vec<pair<T, int>> d(n);
   auto check_edge = [&](int u, int v) {
-    return (b[u] and M1.oracle(u, v)) ||
-           (b[v] and M2.oracle(v, u));
+    return (b[u] && M1.oracle(u, v)) ||
+           (b[v] && M2.oracle(v, u));
   };
   while (true) {
     I[0].clear(), I[1].clear();
-    for (int u = 0; u < n; u++) I[b[u]].push_back(u);
-    // I[1] contem o conjunto de tamanho I[1].size() de menor peso
+    rep(u,0,n) I[b[u]].push_back(u);
     M1.build(I[1]), M2.build(I[1]);
-    for (int u = 0; u < n; u++) {
+    rep(u,0,n) {
       target[u] = false, is_inside[u] = false, from[u] = -1;
-      d[u] = {numeric_limits<T>::max(), INF};
+      d[u] = {numeric_limits<T>::max(), INT_MAX};
     }
     deque<T> q;
     sort(all(I[0]), [&](int i, int j) { return w[i] < w[j]; });
@@ -66,12 +64,12 @@ vi weightedMatroidIntersection(
       if (M1.oracle(u)) {
         if (is_inside[u]) continue;
         d[u] = {w[u], 0};
-        if (!q.empty() and d[u] > d[q.front()]) q.push_back(u);
+        if (!q.empty() && d[u] > d[q.front()]) q.push_back(u);
         else q.push_front(u);
         is_inside[u] = true;
       }
-    }	
-    while (q.size()) {
+    }
+    while (sz(q)) {
       int u = q.front(); q.pop_front();
       is_inside[u] = false;
       for (int v : I[!b[u]]) if (check_edge(u, v)) {
@@ -79,16 +77,16 @@ vi weightedMatroidIntersection(
         if (nd < d[v]) {
           from[v] = u, d[v] = nd;
           if (is_inside[v]) continue;
-          if (q.size() and d[v] > d[q.front()]) q.push_back(v);
+          if (sz(q) && d[v] > d[q.front()]) q.push_back(v);
           else q.push_front(v);
           is_inside[v] = true;
         }
       }
     }
-    pair<T, int> mini = pair(numeric_limits<T>::max(), INF);
+    pair<T, int> mini = pair(numeric_limits<T>::max(), INT_MAX);
     int targ = -1;
-    for (int u : I[0]) if (target[u] and d[u] < mini) 
-      mini = d[u], targ = u;	
+    for (int u : I[0]) if (target[u] && d[u] < mini) 
+      mini = d[u], targ = u;
     if (targ != -1) for (int u = targ; u != -1; u = from[u])
       b[u] = !b[u], w[u] *= -1;
     else break;
--- lib/numerical/ntt.h
+++ lib/numerical/ntt.h
@@ -19,50 +19,50 @@ struct FFT {
   }
   constexpr u red(u k, u a) { return a-k*(a>=k); }
   FFT() {
-    u k = r[2<<LG] = -p%p, b=root, e = p>>LG;
-    for(; e; e/=2, b=m(b,b)) if(e%2) k=m(k, b);
-    for(int i = (2<<LG) - 1; i >= 0; i--)
+    u k = r[2<<LG] = -p%p, b = root, e = p>>LG;
+    for (; e; e /= 2, b = m(b, b)) if (e % 2) k = m(k, b);
+    for (int i = (2<<LG) - 1; i >= 0; i--)
       r[i] = red(p, m(r[i+1], k)), i&(i-1) ? 0 : k = m(k, k);
     assert(r[2] != r[3]); assert(r[1] == r[2]);
   }
-  vec<ll> cv(const vec<ll> &as, const vec<ll> &bs, u *v) {
-    int c=max(sz(as)+sz(bs)-1, 0), n=1;
+  vec<ll> cv(const vec<ll>& as, const vec<ll>& bs, u* v) {
+    int c = max(sz(as) + sz(bs) - 1, 0), n = 1;
     assert(c <= (1<<LG));
-    u h=u(uu(-p)*-p%p), a=m(h, p/2+1), x, y;
-    while(n<c) n*=2, h=red(p, m(h, a));
+    u h = u(uu(-p)*-p%p), a = m(h, p/2 + 1), x, y;
+    while (n < c) n *= 2, h = red(p, m(h, a));
     rep(i,0,n)
-      v[i] = i<sz(as) ? u(as[i]) : 0,
-      v[i+n] = i<sz(bs) ? u(bs[i]) : 0;
-    for(auto s:{v,v+n})
-    for(int j = n; j >= 2; j--)
-    for(int k=j&-j; k/=2;) rep(i,j-k,j)
-      x=s[i], y=s[i-k],
-      s[i-k] = red(2*p, x+y),
-      s[i] = m(2*p+y-x, r[3*k-j+i]);
+      v[i] = i < sz(as) ? u(as[i]) : 0,
+      v[i+n] = i < sz(bs) ? u(bs[i]) : 0;
+    for (auto s : {v, v + n})
+    for (int j = n; j >= 2; j--)
+    for (int k = j&-j; k /= 2;) rep(i,j-k,j)
+      x = s[i], y = s[i-k],
+      s[i-k] = red(2*p, x + y),
+      s[i] = m(2*p + y - x, r[3*k-j+i]);
     rep(i,0,n) v[i] = m(v[i], v[i+n]);
-    rep(j,2,n+1) for(int k=1; !(k&j); k*=2) rep(i,j-k,j)
+    rep(j,2,n+1) for (int k = 1; !(k&j); k *= 2) rep(i,j-k,j)
       x = m(v[i], r[3*k+j-i]),
       y = red(2*p, v[i-k]),
-      v[i-k]=x+y, v[i]=2*p+y-x;
+      v[i-k] = x + y, v[i] = 2*p + y - x;
     rep(i,0,c) v[i] = red(p, m(v[i], h));
-    return vec<ll>(v, v+c);
+    return vec<ll>(v, v + c);
   }
 };
 // for modular convolutions modulo 998244353:
-vec<ll> convSmall(const vec<ll> &as, const vec<ll> &bs) {
+vec<ll> convSmall(const vec<ll>& as, const vec<ll>& bs) {
   static uint32_t v[2<<LG];
   static FFT<uint32_t, uint64_t, 998244353, 3> fft;
   return fft.cv(as, bs, v);
 }
 // for modular convolutions modulo a 62 bit prime:
-vec<ll> convBig(const vec<ll> &as, const vec<ll> &bs) {
+vec<ll> convBig(const vec<ll>& as, const vec<ll>& bs) {
   static uint64_t v[2<<LG];
   static const uint64_t P = (1ull<<62)-(18ull<<32)+1;
   static FFT<uint64_t, __uint128_t, P, 3> fft;
   return fft.cv(as, bs, v);
 }
 // for modular convolutions modulo an arbitrary 32-bit modulus:
-vec<ll> convSunzi(const vec<ll> &v1, const vec<ll> &v2, ll m) {
+vec<ll> convSunzi(const vec<ll>& v1, const vec<ll>& v2, ll m) {
   const uint64_t inv = 2703402103339935109ull,
     mod1 = (1ull<<62)-(18ull<<32)+1,
     mod2 = (1ull<<62)-(76ull<<32)+1;
@@ -70,9 +70,9 @@ vec<ll> convSunzi(const vec<ll> &v1, const vec<ll> &v2, ll m) {
   static uint64_t v[2<<LG];
   static FFT<uint64_t, __uint128_t, mod1, 3> fft1;
   static FFT<uint64_t, __uint128_t, mod2, 17> fft2;
-  auto as=fft1.cv(v1, v2, v), bs=fft2.cv(v1, v2, v);
+  auto as = fft1.cv(v1, v2, v), bs = fft2.cv(v1, v2, v);
   rep(i,0,sz(as)) {
-    auto d = fft1.m(mod1+as[i]-bs[i], inv);
+    auto d = fft1.m(mod1 + as[i] - bs[i], inv);
     d -= mod1*(d >= mod1); d %= m;
     as[i] = (bs[i] + mod2%m*d)%m;
   }
