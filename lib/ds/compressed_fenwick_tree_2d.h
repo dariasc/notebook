b3e461c6a79b08ab214e498b4b20ba5031e29367
Diego Arias <git@dariasc.com>
2026-09-05 00:36:45 -0400

    ds: remove ft, recursive st, persistent st from pdf

--- lib/ds/compressed_fenwick_tree_2d.h
+++ lib/ds/compressed_fenwick_tree_2d.h
@@ -5,9 +5,8 @@ name = "Compressed Fenwick Tree 2D"
 description = "2D fenwick tree with compressed $y$ axis, with point addition and sum range queries. Call `fakeUpdate` on the positions that will receive updates before `init`."
 time = "$O(log^2 n)$"
 - */
-#include "fenwick_tree.h"
 struct FT2 {
-  vec<vi> ys; vec<FT> ft;
+  vec<vi> ys; vec<vec<ll>> ft;
   FT2(int lim_x) : ys(lim_x) {}
   void fakeUpdate(int x, int y) {
     for (; x < sz(ys); x |= x + 1) ys[x].push_back(y);
@@ -20,12 +19,12 @@ struct FT2 {
   }
   void update(int x, int y, ll dif) {
     for (; x < sz(ys); x |= x + 1)
-      ft[x].update(ind(x, y), dif);
+      for (int i = ind(x, y); i < sz(ft[x]); i |= i + 1) ft[x][i] += dif;
   }
   ll query(int x, int y) {
     ll sum = 0;
     for (; x; x &= x - 1)
-      sum += ft[x-1].query(ind(x-1, y));
+      for (int i = ind(x - 1, y); i; i &= i - 1) sum += ft[x - 1][i - 1];
     return sum;
   }
 };
